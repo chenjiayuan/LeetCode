dabacbb8a124e4c616a87a9e6e7dc15d14dac8b3
Jiayuan Chen <chenjiayuancz@gmail.com>
2015-08-04 23:09:34 +0800

    testing readme in sub directory on Github

--- Palindrome_Number/solution.rb
+++ Palindrome_Number/solution.rb
@@ -1,15 +1,15 @@
-ram {Integer} x
+# @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
     return false if x < 0
     return true if x < 10
-    
-    y = 0
+    reverse = 0
     tmp = x
-    begin
-        y = (10 * y) + (tmp % 10)
+    
+    while tmp != 0
+        reverse = (10 * reverse) + (tmp % 10)
         tmp /= 10
-    end until tmp == 0
-    return x == y
+    end
+    
+    return reverse == x
 end
-
